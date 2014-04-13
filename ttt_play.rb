2ea3295e3e37935d68e375a262532b35d4c3a90f
Sheri Wilson <smgwilson@gmail.com>
2014-04-13 15:10:24 -0700

    Fixes program flow for user turn
    -removed @valid_input instance variable
    -added valid_input? method
    -removed "X goes first" statement
    -added exit method
    -revised flow of user_turn to utilize new methods

--- ttt_play.rb
+++ ttt_play.rb
@@ -4,12 +4,10 @@ require './ttt_game.rb'
 @cpu = "Computer"
 @human_marker = ""
 @cpu_marker = ""
-@valid_input = [0..9]
 
 @board = {:a1=>"1", :a2=>"4", :a3=>"7",
     :b1=>"2", :b2=>"5", :b3=>"8",
     :c1=>"3", :c2=>"6", :c3=>"9"}
-    
 
 puts "Let's Play Tic-Tac_Toe!"
 print "What is your name?: "
@@ -32,11 +30,15 @@ def draw_game
   puts "#{@cpu}: #{@cpu_marker}"
   puts "#{@human}: #{@human_marker}"
   puts ""
-  puts "X goes first."
+end
+
+def valid_input?(input)
+  valid = (0..9).to_a
+  valid.include?(input.to_i)
 end
 
 def invalid_move
-  puts "Invalid move!  Choose an empty space by number"
+  puts "Invalid move! Select a space by number."
 end
 
 def next_turn(current_player)
@@ -48,21 +50,26 @@ def next_turn(current_player)
 end
 
 def user_turn
- draw_game
- puts "Your turn.  Make a move or type 'q' to quit"
- input = gets
- #if @valid_input.include?(input) 
-  selection = @board.key(input.chomp)
-  @board[selection] = @human_marker
-  next_turn(@human)
- # else
-  #  invalid_move
-  #end
+  draw_game
+  puts "Your turn.  Make a move or type 'q' to quit"
+  input = gets.chomp!
+  if input == "q"
+    puts "Bye for now!"
+    exit
+  else
+    selection = @board.key(input)
+    if valid_input?(input)
+      @board[selection] = @human_marker
+      next_turn(@human)
+    else
+      invalid_move #fix; not going here for string input.
+    end
+  end
 end
 
 def cpu_turn
-    draw_game
-    puts "Computer's turn"
+  draw_game
+  puts "Computer's turn"
 end
 
 if @human_marker == "X"
@@ -71,8 +78,11 @@ else
   cpu_turn
 end
 
-
 def winner?
-  
+
+end
+
+def exit
+  exit 0
 end
 
